fb84b9d57795cf8149de5f45f62650fa856487b6
jshergill <jshergill@algomau.ca>
2025-07-18 19:20:09 +0000

    Breadcrumb bootstrap

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -23,36 +23,17 @@ if (!isset($_SESSION['auth'])) {
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/reminders">Reminders</a>
-        </li>
-        <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
-            <li class="nav-item">
-                <a class="nav-link active" href="/reports">Reports</a>
-            </li>
-        <?php endif; ?>
-
-
-        <li class="nav-item">
-          <a class="nav-link" href="/about">About Me</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="/home">Home</a></li>
+      <li class="breadcrumb-item"><a href="/reminders">Reminders</a></li>
+   <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
+      <li class="breadcrumb-item"><a href="/reports">Reports</a></li>
+          <li class="breadcrumb-item"><a href="/logout">Logout</a></li>
+  <?php endif; ?>
+         
+      
+        
       </ul>
     </div>
   </div>
